cb7e7c00801c3a3c140fc3d82e04f1da1535ed2c
PriyaLollmun <72708118+PriyaLollmun@users.noreply.github.com>
2020-12-04 18:14:30 -0400

    Update help.js

--- Project 3 freind version/scripts/helps/help.js
+++ Project 3 freind version/scripts/helps/help.js
@@ -3,23 +3,28 @@
  * Kinshuk Chadha (A00431288)
  * Alexander Lamey (A00410007)
  * Priya Lollmun (A00430148)
+ * 
+ * Revised by Priya Lollmun 
+ * 
  * help.js:
  * js file that is used by the help screens to load the messages.
  */
 
 /*
  * Below are the variables for each message.
- * They should be loaded onto the server at some point and fetched.
- * For now we are using constants.
  */
 const BODY_MESSAGE =
-  "<br>" +
   "<p>1) How should you greet the person you are emailing?<p>" +
-  "<br><br>" +
+  "<p> For example: You can use 'hello' for professors and 'hi' for family and friends.<p>" +
+  "<p> It is important to know the difference between informal and formal salutations.<p>" +
   "<p>2) Do you need any questions?<p>" +
-  "<br><br>" +
   "<p>3) Does the person you are emailing need to know any " +
-  "information about you?<p>";
+  "information about you?<p>" +
+  "<p>4) Example of an email:<br><br>" +
+  "Hello,<br><br>" +
+  "How are you doing today?<br><br>" +
+  "Regards,<br><br>" +
+  "(insert your own name here)";
 
 const CC_MESSAGE =
   "<br>" + "<p>Is there anyone you need to copy on this email?<p>" + "<br>";
